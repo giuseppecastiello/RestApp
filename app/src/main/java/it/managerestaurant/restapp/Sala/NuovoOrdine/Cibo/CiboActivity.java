50e54b36b353be8bcb7494e6f518ccbbaa1c75b4
giuseppecastiello <giuseppecastiello@outlook.com>
2019-09-12 16:02:33 +0200

    primo client funzionante(con warnings)

--- app/src/main/java/it/managerestaurant/restapp/Sala/NuovoOrdine/Cibo/CiboActivity.java
+++ app/src/main/java/it/managerestaurant/restapp/Sala/NuovoOrdine/Cibo/CiboActivity.java
@@ -3,6 +3,8 @@ package it.managerestaurant.restapp.Sala.NuovoOrdine.Cibo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,19 +12,46 @@ import it.managerestaurant.restapp.MainActivity;
 import it.managerestaurant.restapp.R;
 import it.managerestaurant.restapp.Sala.NuovoOrdine.NuovoOrdineActivity;
 
+
 public class CiboActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cibo);
-    }
 
-    public void openMain(View view){
+        // identifico il campo EditText
+        final EditText risposta = findViewById(R.id.editTextInserisci);
+        // identifico il bottone
+        Button btnInvia = findViewById(R.id.buttonInvia);
+
+        btnInvia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                //String url = "http://sbaccioserver.ddns.net:8080/prodotto";
+                AsyncTaskGet task = new AsyncTaskGet();
+                task.execute();
+                try {
+                    while (task.ready == false) {
+                        Thread.sleep(100);
+                    }
+                    System.out.println("Non mi sono inchiodato");
+                    risposta.append(task.json);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+        public void openMain(View view){
         Intent openMain = new Intent(this, MainActivity.class);
         startActivity(openMain);
     }
+
     public void openNuovoOrdine(View view){
         Intent openNuovoOrdine = new Intent(this, NuovoOrdineActivity.class);
         startActivity(openNuovoOrdine);
     }
+
 }
