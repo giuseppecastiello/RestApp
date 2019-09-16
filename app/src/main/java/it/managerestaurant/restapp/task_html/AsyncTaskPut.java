fbe95f8c1823f7d9b051acfdff825a2d47534875
giuseppecastiello <giuseppecastiello@outlook.com>
2019-09-17 00:15:08 +0200

    aggiunti tutti i task

---
+++ app/src/main/java/it/managerestaurant/restapp/task_html/AsyncTaskPut.java
@@ -0,0 +1,35 @@
+package it.managerestaurant.restapp.task_html;
+
+import android.os.AsyncTask;
+
+import com.mashape.unirest.http.Unirest;
+
+public class AsyncTaskPut extends AsyncTask<String, Void, Boolean> {
+	public String json = "";
+	public boolean ready = false;
+	private String url = "";
+
+	@Override
+	protected void onPreExecute () {
+		super.onPreExecute();
+	}
+
+	public void setUri(String uri){
+		this.url = "http://sbaccioserver.ddns.net:8081/" + uri;
+	}
+
+	@Override
+	protected Boolean doInBackground (String...urls) {
+		try {
+			json = Unirest.put(url).asString().getBody();
+			System.out.println(json);
+			ready = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	protected void onPostExecute (Boolean result){
+
+	}
+}
